88c8248284701337c21699eca96ff30607ac7bb6
zaids2641 <39379399+zaids2641@users.noreply.github.com>
2021-03-10 15:45:18 +0800

    First Commit

---
+++ assets/scripts.js
@@ -0,0 +1,13 @@
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+  
+  function hoverPlay() {
+    var audio = document.getElementById("hoveraudio");
+    audio.play();
+  }
+function playAudio() {
+    var audio = document.getElementById("playaudio");
+    audio.play();
+  }
